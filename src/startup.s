7a7188763fc370ed5032f0b55ee01710b4f43305
fred13kim <fred13kim@gmail.com>
2025-07-25 00:31:25 -0400

    Add premature startup assembly script

--- src/startup.s
+++ src/startup.s
@@ -1,9 +1,39 @@
-.syntax unified
-.cpu cortex-m4
-.thumb
+    .syntax unified
+    .cpu cortex-m4
+    .thumb
 
-.section .text.Reset_Handler
-.type Reset_Handler, %function
-.global Reset_Handler
+/* ---------------------------------------- */
+
+    .global     Vectors
+    .global     Reset_Handler
+
+/* ---------------------------------------- */
+
+    .section    .text.Reset_Handler
+    .type       Reset_Handler, %function
+
 Reset_Handler:
-    nop
+    ldr sp, =_mstack /* LOAD main stack address into stack pointer */
+
+/* ---------------------------------------- */
+
+    .section    .isr_vector
+
+Vectors:
+    .word   _mstack
+    .word   Reset_Handler
+    .word   NMI_Handler
+    .word   Hard_Fault_Handler
+
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
+    .word   0
