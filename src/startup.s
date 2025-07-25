60d9d9d1e095398f792108e2620c9bf375ff95d7
fred13kim <fred13kim@gmail.com>
2025-07-25 04:06:17 -0400

    Add initialization of data and bss

--- src/startup.s
+++ src/startup.s
@@ -2,14 +2,27 @@
  * startup.S
  */
 
+/* ---------------------------------------- */
+
     .syntax unified
     .cpu cortex-m4
     .thumb
 
 /* ---------------------------------------- */
 
-    .global     Vectors
+    .global     SystemInit
     .global     Reset_Handler
+    .global     Vectors
+
+/* ---------------------------------------- */
+    .section    .text.SystemInit
+    .type       SystemInit, %function
+
+SystemInit:
+    /* Not relocating vector table */
+    /* Not using fpu */
+    /* Not configuring system clock yet */
+    bx      lr
 
 /* ---------------------------------------- */
 
@@ -17,7 +30,48 @@
     .type       Reset_Handler, %function
 
 Reset_Handler:
-    ldr sp, =_mstack /* LOAD main stack address into stack pointer */
+    ldr     sp, =_mstack /* LOAD main stack address into stack pointer */
+    bl      SystemInit
+
+    /* Copy initialized data from VMA (flash) to LMA (sram) */
+
+    ldr     r0, =_sdata
+    ldr     r1, =_edata
+    ldr     r2, =_sidata
+
+    movs    r3, #0
+    b       LoopCopyDataInit
+
+CopyDataInit:
+    ldr     r4, [r2, r3]
+    str     r4, [r0, r3]
+    adds    r3, r3, #4
+
+LoopCopyDataInit:
+    adds    r4, r0, r3
+    cmp     r4, r1
+    bcc     CopyDataInit
+
+    /* Zero fill BSS region */
+
+    ldr     r0, =_sbss
+    ldr     r1, =_ebss
+
+    movs    r3, #0
+    b       LoopZeroBSS
+
+ZeroBSS:
+    str     r3, [r0]
+    adds    r0, r0, #4
+
+LoopZeroBSS:
+    cmp     r1, r0
+    bcc     ZeroBSS
+
+
+
+
+
 
 /* ---------------------------------------- */
 
@@ -26,19 +80,20 @@ Reset_Handler:
 /* Defined in B1.5.2 of ARMv7-M Ref */
 /*                                  ADDR    */
 Vectors:
-    .word   _mstack                 //x00
-    .word   Reset_Handler           //x04
-    .word   NMI_Handler             //x08
-    .word   HardFault_Handler       //x0c
-    .word   MemManage_Handler       //x10
-    .word   BusFault_Handler        //x14
-    .word   UsageFault_Handler      //x18
-    .word   0                       //x1c
-    .word   0                       //x20
-    .word   0                       //x24
-    .word   0                       //x28
-    .word   SVC_Handler             //x2c
-    .word   DebugMon_Handler        //x30
-    .word   0                       //x34
-    .word   PendSV_Handler          //x38
-    .word   SysTick_Handler         //x3c
+    .word   _mstack                 //x000
+    .word   Reset_Handler           //x004
+    # .word   NMI_Handler             //x008
+    # .word   HardFault_Handler       //x00c
+    # .word   MemManage_Handler       //x010
+    # .word   BusFault_Handler        //x014
+    # .word   UsageFault_Handler      //x018
+    .word   0                       //x01c
+    .word   0                       //x020
+    .word   0                       //x024
+    .word   0                       //x028
+    # .word   SVC_Handler             //x02c
+    # .word   DebugMon_Handler        //x030
+    .word   0                       //x034
+    # .word   PendSV_Handler          //x038
+    # .word   SysTick_Handler         //x03c
+    # .word   _IRQ_Handler           //x040
