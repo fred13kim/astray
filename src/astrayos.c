bc6fa2a72567c4cb07ee9b51ce8b4e1aac863d9b
fred13kim <fred13kim@gmail.com>
2025-07-27 15:34:02 -0400

    Continue systick config

--- src/astrayos.c
+++ src/astrayos.c
@@ -3,19 +3,35 @@
 
 void SysTick_Handler(void)
 {
-	uint32_t val = SYSTICK->VAL;
+	uint32_t val = SYSTICK->CVR;
 	(void)val;
 }
 
-void ASTRAYOS_init(void)
+uint32_t ASTRAYOS_SYSTICK_init(uint32_t ticks)
 {
+	if ((ticks - 1UL) > SYSTICK_RVR_RELOAD_MASK) {
+		return 1;
+	}
 	/**
      * Initialize systick
      */
 
+	/* SYSTICK should and is disabled by default */
+
 	/* Load the reload value */
-	SYSTICK->RVR = ((SYS_CLK_FREQ / 10UL) - 1UL) & SYSTICK_RVR_RELOAD_MASK;
+	SYSTICK->RVR = ((SYS_CLK_FREQ / 10UL) - 1UL);
+
+	/* Clear the systick timer */
+	SYSTICK->CVR = 0UL;
 
 	/* Enable systick timer with processor clk & pending interrupts enabled */
 	SYSTICK->CSR = SYSTICK_CSR_CLKSRC | SYSTICK_CSR_TICKINT | SYSTICK_CSR_EN;
+
+	return 0;
+}
+
+void ASTRAYOS_init(void)
+{
+	/* Initialize systick period to 1 ms */
+	ASTRAYOS_SYSTICK_init(SYS_CLK_FREQ / 1000UL);
 }
