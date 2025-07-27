e1574188492693d5e78ad8161fd33130c4bc3376
fred13kim <fred13kim@gmail.com>
2025-07-27 16:41:03 -0400

    Add vector table

--- core/core.h
+++ core/core.h
@@ -19,10 +19,10 @@
 
 /* Register map */
 
-#define ITM_BASE     (0xE0000000UL) /* Instruction Trace Macrocell */
-#define DWT_BASE     (0xE0001000UL) /* Data Watchpoint and Trace */
-#define FPB_BASE     (0xE0002000UL) /* Flashpatch and Breakpoint */
-#define SCS_BASE     (0xE000E000UL) /* System Control Space */
+#define ITM_BASE     (0xE0000000UL)        /* Instruction Trace Macrocell */
+#define DWT_BASE     (0xE0001000UL)        /* Data Watchpoint and Trace */
+#define FPB_BASE     (0xE0002000UL)        /* Flashpatch and Breakpoint */
+#define SCS_BASE     (0xE000E000UL)        /* System Control Space */
 #define SYSTICK_BASE (SCS_BASE + 0x0010UL) /* System Timer */
 #define NVIC_BASE    (SCS_BASE + 0x0100UL) /* Nested Vector Interrupt Controller */
 #define SCB_BASE     (SCS_BASE + 0x0D00UL) /* System Control Block */
