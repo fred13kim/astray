7e896c0fc8a253920b479abb1fad7bfdd6000555
fred13kim <fred13kim@gmail.com>
2025-07-31 18:34:11 -0400

    Update clang-format

--- core/core.h
+++ core/core.h
@@ -19,13 +19,13 @@
 
 /* Register map */
 
-#define ITM_BASE     (0xE0000000UL)        /* Instruction Trace Macrocell */
-#define DWT_BASE     (0xE0001000UL)        /* Data Watchpoint and Trace */
-#define FPB_BASE     (0xE0002000UL)        /* Flashpatch and Breakpoint */
-#define SCS_BASE     (0xE000E000UL)        /* System Control Space */
+#define ITM_BASE	 (0xE0000000UL)		   /* Instruction Trace Macrocell */
+#define DWT_BASE	 (0xE0001000UL)		   /* Data Watchpoint and Trace */
+#define FPB_BASE	 (0xE0002000UL)		   /* Flashpatch and Breakpoint */
+#define SCS_BASE	 (0xE000E000UL)		   /* System Control Space */
 #define SYSTICK_BASE (SCS_BASE + 0x0010UL) /* System Timer */
-#define NVIC_BASE    (SCS_BASE + 0x0100UL) /* Nested Vector Interrupt Controller */
-#define SCB_BASE     (SCS_BASE + 0x0D00UL) /* System Control Block */
+#define NVIC_BASE	 (SCS_BASE + 0x0100UL) /* Nested Vector Interrupt Controller */
+#define SCB_BASE	 (SCS_BASE + 0x0D00UL) /* System Control Block */
 
 /* SYSTICK Register map */
 
@@ -38,9 +38,9 @@ typedef struct {
 	__RO uint32_t CALIB;
 } SYSTICK_T;
 
-#define SYSTICK_CSR_CLKSRC      (1UL << 2)
-#define SYSTICK_CSR_TICKINT     (1UL << 1)
-#define SYSTICK_CSR_EN          (1UL << 0)
+#define SYSTICK_CSR_CLKSRC		(1UL << 2)
+#define SYSTICK_CSR_TICKINT		(1UL << 1)
+#define SYSTICK_CSR_EN			(1UL << 0)
 #define SYSTICK_RVR_RELOAD_MASK ((1UL << 24) - 1UL)
 
 #endif // !CORE_PERIPHS_H
--- inc/list.h
+++ inc/list.h
@@ -1,16 +1,29 @@
 #ifndef LIST_H
 #define LIST_H
 
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
 
-typedef struct list_entry_s {
-	struct list_entry_s *next;
-	struct list_entry_s *prev;
-	void                *data;
-} list_entry_t;
+#define LIST_HEAD(name) struct list_head name = LIST_HEAD_INIT(name)
 
-typedef struct list_s {
-	list_entry_t *head;
-} list_t;
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+	list->next = list;
+	list->prev = list;
+}
+
+static inline void __list_add(struct list_head *new, struct list_head *prev,
+							  struct list_head *next)
+{
+}
+
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head, head->next);
+}
 
 
 #endif // !LIST_H
