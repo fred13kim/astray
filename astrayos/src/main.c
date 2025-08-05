85a0b2bab53d6b23482519773322f6f5c3a1556e
fred13kim <fred13kim@gmail.com>
2025-08-04 21:19:07 -0400

    Add basic task initialization

--- astrayos/src/main.c
+++ astrayos/src/main.c
@@ -9,7 +9,19 @@ int	 a, b, c, d;
 int main(void)
 {
 	ASTRAYOS_init();
-	init_task();
+
+	ll_node_t tasks;
+	ll_list_init(&tasks);
+
+
+	task_t task_a;
+	task_init(&task_a, "Task A", 1);
+
+	task_t task_b;
+	task_init(&task_b, "Task B", 2);
+
+	ll_list_add(&tasks, &task_a.node);
+	ll_list_add(&tasks, &task_b.node);
 
 	while (1) {
 		;
